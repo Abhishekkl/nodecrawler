4141375fa343fe042192eb19651a8d73f3ee9880
Abhishekkl <35161597+Abhishekkl@users.noreply.github.com>
2022-02-08 23:07:46 +0530

    Add files via upload
    web crawl

---
+++ index.js
@@ -0,0 +1,31 @@
+var Crawler = require("crawler");
+
+const argv = process.argv;
+const url = argv[2].split('=')[1]
+const words = (argv[3].toLowerCase().split('=')[1]).split(',')
+
+const findWordCount = (body) => {
+    let searchedobj = {}
+    body.map((val)=>{
+        if(words.includes(val.toLowerCase())){
+            searchedobj[val] = searchedobj[val] ? ++searchedobj[val] : 1
+        }
+    })
+    return searchedobj;
+}
+
+const crawler = new Crawler({
+    maxConnections : 10,
+    callback : function (error, res, done) {
+        if(error){
+            console.log(error);
+        }else{
+            const $ = res.$;
+            let body = $("body").text().split(/\s+/);
+            console.log(findWordCount(body))
+        }
+        done();
+    }
+});
+
+crawler.queue(url);
